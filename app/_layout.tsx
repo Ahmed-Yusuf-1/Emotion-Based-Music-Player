f246fd8fc4b5752d7ab4c0e383c205b88b9d41bb
Ahmed Yusuf <ahned.ib.695@gmail.com>
2025-09-08 01:01:44 -0600

    Add sign-in screen and custom fonts
    Introduced a new sign-in screen with username and password fields, guest access, and custom font styling. Added Retro Vintage and Valestine font files and updated app configuration to load these fonts. Modified layout to include the sign-in route.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
+      <Stack.Screen name="signin" options={{headerShown: false}}/>
       <Stack.Screen name="(tabs)" options={{headerShown: false }} />
-      <Stack.Screen name="index" options={{headerShown: false }} />
       <Stack.Screen name="capture" options={{headerShown: false}}/>
     </Stack>
   );
